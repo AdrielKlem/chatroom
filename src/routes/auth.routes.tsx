12429eba6dc6037949b60846d16f392a6b8eb702
Adriel Klem <adrielklem16@gmail.com>
2024-02-20 18:09:49 -0300

    initial project

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,6 @@
+import { } from 'react-router-dom'
+
+
+export default function AuthRoutes() {
+    console.log()
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { BrowseRouter } from 'react-router-dom'
+
+import { AuthRoutes } from './auth.routes'
+
+export default function Routes() {
+    return(
+        <BrowserRouter>
+            <AuthRoutes />
+        </BrowserRouter>
+    )
+}
