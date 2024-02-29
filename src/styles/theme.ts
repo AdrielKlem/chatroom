f6335d92a6cc5ae0c1acd6ad6c245e67b70b89fb
Adriel Klem <adrielklem16@gmail.com>
2024-02-29 20:18:28 -0300

    add colors themes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,30 @@
-export default {
-    
+
+export const blueDarkTheme = {   
+    COLORS: {
+        WHITE: "#FFF",
+        BACK_001: "#AABCD0",
+        BACK_002: "#00d6d6",
+        BACK_003: "#000",
+        COLOR_ACTION: "#01AB5B1",
+    }
+}
+        
+export const orangeTheme = {
+    COLORS: {
+        WHITE: "#FFF",
+        BACK_001: "#CCC6BA",
+        BACK_002: "#3F3C37",
+        BACK_003: "#201F1B",
+        COLOR_ACTION: "#FF5E1E",
+    }
+}
+
+export const blueLightTheme = {
+    COLORS: {
+        WHITE: "#FFF",
+        BACK_001: "#F4F3EE",
+        BACK_002: "#131E2F",
+        BACK_003: "#000",
+        COLOR_ACTION: "#3A89FF",
+    }
 }
