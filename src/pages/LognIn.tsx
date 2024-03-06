175168bc5409cef6a16653a303f20c32807ce972
Adriel Klem <adrielklem16@gmail.com>
2024-03-06 19:36:53 -0300

    fixed pages LognIn

--- src/pages/LognIn.tsx
+++
@@ -1,7 +0,0 @@
-export function LognIn() {
-    return (
-        <>
-        <h1>Hello, World</h1>
-        </>
-    )
-}
---
+++ src/pages/LognIn/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles"
+
+
+export function LognIn() {
+    return (
+        <Container>
+            <h1>Trocar o Estilo</h1>
+            <button>Clicar</button>
+        </Container>
+    )
+}
