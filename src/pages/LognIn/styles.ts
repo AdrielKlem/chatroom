b8fc03407501e8dde112cf5049bc1784066deac8
Adriel Klem <adrielklem16@gmail.com>
2024-03-10 18:48:48 -0300

    update style to change theme

--- src/pages/LognIn/styles.ts
+++ src/pages/LognIn/styles.ts
@@ -2,4 +2,17 @@ import styled from "styled-components";
 
 
 export const Container = styled.div`
+    background-color: #131E2F;
+
+    margin: 8.5rem auto 0;
+    padding: 1.2rem;
+    height: 70vh;
+    width: 76rem;
+
+    border-radius: 5rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
 `
