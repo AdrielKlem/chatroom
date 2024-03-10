130b817928ccfad1e81561412da96013572104cd
Adriel Klem <adrielklem16@gmail.com>
2024-03-10 18:24:28 -0300

    update button style

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,5 +2,19 @@ import styled from "styled-components";
 
 
 export const Container = styled.button`
-    background-color: blue;
+    background-color: #00d6d6;
+
+    margin: 0 auto;
+    padding: 1.2rem;
+
+    border-radius: 1.2rem;
+    
+    color: #fff;
+
+    transition: ease-in .5s;
+    
+    &:hover {
+        filter: drop-shadow(0 0 1.6rem #00d6d6);
+        transform: scale(1.05);
+    }
 `
