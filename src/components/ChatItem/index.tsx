b391ba89f8d4e8a05056cd74b3cdd86f9db98a52
Adriel Klem <adrielklem16@gmail.com>
2024-07-31 20:11:39 -0300

    update details

--- src/components/ChatItem/index.tsx
+++ src/components/ChatItem/index.tsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
-import { Container, PictureUser, DetailsUser, NameUser, LastMessage, DetailsTime } from "./styles"
+import { Container, PictureUser, DetailsUser, NameUser, LastMessage, DetailsBox, DetailsTime } from "./styles"
 
 function IconView() {
   return <FontAwesomeIcon icon={faCheck} style={{color: "#74C0FC",}} />;
@@ -24,14 +24,16 @@ export function ChatItem({data}: dataProps) {
                     src={data.pictureUser}
                     alt="Foto usuário" />
             </PictureUser>
-            <DetailsUser>
-                <NameUser>{data.username}</NameUser>
-                <LastMessage>{data.lastMessage}</LastMessage>
-            </DetailsUser>
-            <DetailsTime>
-                {data.lastTime}
-                <IconView />
-            </DetailsTime>
+            <DetailsBox>
+                <DetailsUser>
+                    <NameUser>{data.username}</NameUser>
+                    <LastMessage>{data.lastMessage}</LastMessage>
+                </DetailsUser>
+                <DetailsTime>
+                    {data.lastTime}
+                    <IconView />
+                </DetailsTime>
+            </DetailsBox>
         </Container>
     )
 }
--- src/components/ChatItem/styles.ts
+++ src/components/ChatItem/styles.ts
@@ -4,15 +4,21 @@ import { styled } from "styled-components"
 export const Container = styled.div`
     width: auto;
     display: flex;
-`
+
+    padding: 1rem;
+    `
 
 export const PictureUser = styled.div`
     > img {
         width: 4rem;
         height: 4rem;
-
+        
         border-radius: 50%;
     }
+`
+
+export const DetailsBox = styled.div`
+    border-bottom: 1px solid #000;
 `
 
 export const DetailsUser = styled.div`
