03a111ed0c6fe9ac8d6d3a54b1b82a28de215b55
Adriel Klem <adrielklem16@gmail.com>
2024-09-07 21:15:53 -0300

    update MessageItem

--- src/components/MessageItem/styles.ts
+++ src/components/MessageItem/styles.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
+    background-color: ${({ theme }) => theme.colors.secondaryBackground};
+
+    height: auto;
+    max-width: 25%;
+    padding: 1.6rem;
+    border-radius: .8rem;
 `
